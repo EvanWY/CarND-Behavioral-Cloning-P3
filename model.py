3f010a15d947e6b19b84efff4fe770484de886d2
Wang Yang <wangyang.evan@qq.com>
2017-12-25 02:17:38 -0800

    awa

--- model.py
+++ model.py
@@ -18,13 +18,46 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
-
-print ()
+from keras.layers import Flatten, Dense, Lambda, Activation, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from keras.utils.layer_utils import print_layer_shapes
 
 model = Sequential()
-model.add(Flatten(input_shape = X_train.shape[1:]))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape = X_train.shape[1:]))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(24, 5, 5))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(36, 5, 5))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(48, 5, 5))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(50))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
 model.add(Dense(1))
+
+print_layer_shapes(model, input_shape = X_train.shape[1:])
 
 model.compile(loss = 'mse', optimizer = 'adam')
 model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 3)
