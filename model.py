b1016009eee9ca9157542866d5c9b5a3b433f890
Wang Yang <wangyang.evan@qq.com>
2017-12-27 06:09:02 -0800

    found the single most important optimization

--- model.py
+++ model.py
@@ -15,47 +15,65 @@ with open('records/driving_log.csv') as csvfile:
     for line in reader:
         samples.append(line)
 
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+train_samples, validation_samples = train_test_split(samples, test_size=0.1)
 
-def generator(samples, batch_size=32):
+def generator(samples, batch_size=36):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         sklearn.utils.shuffle(samples)
-        for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
+        for offset in range(0, num_samples, batch_size // 6):
+            batch_samples = samples[offset:offset + batch_size // 6]
 
             images = []
             angles = []
             for batch_sample in batch_samples:
                 frame_angle = float(batch_sample[3])
-                images.append(cv2.imread('records/IMG/'+batch_sample[0].split('/')[-1]))
+                
+                img = cv2.imread('records/IMG/'+batch_sample[0].split('/')[-1])
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                img_flip = cv2.flip(img, 1)
+                images.append(img)
                 angles.append(frame_angle)
-                images.append(cv2.imread('records/IMG/'+batch_sample[1].split('/')[-1]))
-                angles.append(frame_angle + 0.05)
-                images.append(cv2.imread('records/IMG/'+batch_sample[2].split('/')[-1]))
-                angles.append(frame_angle - 0.05)
+                images.append(img_flip)
+                angles.append(-frame_angle)
+                
+                img = cv2.imread('records/IMG/'+batch_sample[1].split('/')[-1])
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                img_flip = cv2.flip(img, 1)
+                images.append(img)
+                angles.append(frame_angle + 0.15)
+                images.append(img_flip)
+                angles.append(-frame_angle - 0.15)
+                
+                img = cv2.imread('records/IMG/'+batch_sample[2].split('/')[-1])
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                img_flip = cv2.flip(img, 1)
+                images.append(img)
+                angles.append(frame_angle - 0.15)
+                images.append(img_flip)
+                angles.append(-frame_angle + 0.15)
 
             # trim image to only see section with road
-            X_train = np.array(images)
+            X_train = np.array(images).reshape(-1,160,320,1)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
 # compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+train_generator = generator(train_samples, batch_size=36)
+validation_generator = generator(validation_samples, batch_size=36)
 
 model = Sequential()
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape = (160,320,3)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape = (160,320,1)))
 model.add(Lambda(lambda x: x/255.0 - 0.5))
-model.add(Convolution2D(24, 5, 5, subsample = (3,3), activation = 'relu'))
-model.add(Convolution2D(36, 5, 5, subsample = (2,2), activation = 'relu'))
-model.add(Dropout(0.5))
+model.add(Convolution2D(36, 5, 5, subsample = (3,3), activation = 'relu'))
 model.add(Convolution2D(48, 5, 5, subsample = (2,2), activation = 'relu'))
-model.add(Convolution2D(64, 3, 3, activation = 'relu'))
 model.add(Dropout(0.5))
-model.add(Convolution2D(64, 3, 3, activation = 'relu'))
+model.add(Convolution2D(64, 5, 5, subsample = (2,2), activation = 'relu'))
+model.add(Convolution2D(96, 3, 3, activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Convolution2D(96, 3, 3, activation = 'relu'))
 model.add(Flatten())
-model.add(Dense(100, activation = 'relu'))
+model.add(Dense(240, activation = 'relu'))
 model.add(Dense(50, activation = 'relu'))
 model.add(Dense(1))
 
@@ -64,10 +82,10 @@ print('### train sample size == {}, validation sample size == {}'.format(len(tra
 model.compile(loss = 'mse', optimizer = 'adam')
 model.fit_generator(
     train_generator, 
-    samples_per_epoch = len(train_samples), 
+    samples_per_epoch = len(train_samples) * 6, 
     validation_data = validation_generator, 
-    nb_val_samples = len(validation_samples), 
-    nb_epoch = 200)
+    nb_val_samples = len(validation_samples) * 6, 
+    nb_epoch = 4)
 
 model.save('model.h5')
 exit()
